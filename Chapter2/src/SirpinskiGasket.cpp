b7d0f4a5be4e0150c601a629b7b0b7e978bf611a
electriccheffer <electriccheff@gmail.com>
2025-01-28 05:16:04 -0600

    CHANGED tab spacing

--- Chapter2/src/SirpinskiGasket.cpp
+++ Chapter2/src/SirpinskiGasket.cpp
@@ -10,18 +10,18 @@ int main() {
 		return -1;
 	
 	}
-    GLFWwindow* window = createAWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+	GLFWwindow* window = createAWindow(800, 600, "OpenGL Test", nullptr, nullptr);
     
-    // Render loop
-    while (!glfwWindowShouldClose(window)) {
-        // Clear the screen
-        glClear(GL_COLOR_BUFFER_BIT);
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+        	// Clear the screen
+        	glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap buffers and poll events
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+        	// Swap buffers and poll events
+        	glfwSwapBuffers(window);
+        	glfwPollEvents();
+    	}
 
-    glfwTerminate();
-    return 0;
+    	glfwTerminate();
+    	return 0;
 }
