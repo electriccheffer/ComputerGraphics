098957919c33855fc05fc857483df33490312e73
electriccheffer <electriccheff@gmail.com>
2025-01-28 05:24:15 -0600

    IMPLEMENTED ERROR HANDLING IN SirpinskiGasket.cpp main

--- Chapter2/src/SirpinskiGasket.cpp
+++ Chapter2/src/SirpinskiGasket.cpp
@@ -1,6 +1,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "../include/GLFWFunctions.hpp"
+#include <stdexcept>
 
 int main() {
 	
@@ -10,18 +11,24 @@ int main() {
 		return -1;
 	
 	}
-	GLFWwindow* window = createAWindow(800, 600, "OpenGL Test", nullptr, nullptr);
-    
-	// Render loop
-	while (!glfwWindowShouldClose(window)) {
-        	// Clear the screen
-        	glClear(GL_COLOR_BUFFER_BIT);
+	try{	
+		GLFWwindow* window = createAWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+			// Render loop
+		while (!glfwWindowShouldClose(window)) {
+        		// Clear the screen
+        		glClear(GL_COLOR_BUFFER_BIT);
 
-        	// Swap buffers and poll events
-        	glfwSwapBuffers(window);
-        	glfwPollEvents();
+        		// Swap buffers and poll events
+        		glfwSwapBuffers(window);
+        		glfwPollEvents();
+    		}
+
+    		glfwTerminate();
+    		return 0;
+
     	}
-
-    	glfwTerminate();
-    	return 0;
+	catch(const std::runtime_error& e){
+		std::cout << "Error: " << e.what() << std::endl; 	
+		return -1; 
+	} 
 }
